eab82bab10ca90535f94627a1777cb2d318569e9
kcw0360 <kcw0360@naver.com>
2023-04-12 16:00:27 +0900

    수정/조회 기능 & Test

---
+++ src/main/java/com/kcw0360/study/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.kcw0360.study.springboot.web.dto;
+
+import com.kcw0360.study.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
