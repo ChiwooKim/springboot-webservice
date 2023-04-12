5d8f3cadd3b0fac361008444d0d222ec6ef63d86
kcw0360 <kcw0360@naver.com>
2023-04-12 15:09:55 +0900

    등록 기능 & Test

---
+++ src/main/java/com/kcw0360/study/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.kcw0360.study.springboot.service;
+
+import com.kcw0360.study.springboot.domain.posts.PostsRepository;
+import com.kcw0360.study.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/kcw0360/study/springboot/web/PostApiController.java
@@ -0,0 +1,21 @@
+package com.kcw0360.study.springboot.web;
+
+import com.kcw0360.study.springboot.service.PostsService;
+import com.kcw0360.study.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
