d3953bf491496324452e10da4b48c714553ff2a8
kcw0360 <kcw0360@naver.com>
2023-04-14 14:14:12 +0900

    전체 조회 화면

--- src/main/java/com/kcw0360/study/springboot/service/PostsService.java
+++ src/main/java/com/kcw0360/study/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.kcw0360.study.springboot.service;
 
 import com.kcw0360.study.springboot.domain.posts.Posts;
 import com.kcw0360.study.springboot.domain.posts.PostsRepository;
+import com.kcw0360.study.springboot.web.dto.PostsListResponseDto;
 import com.kcw0360.study.springboot.web.dto.PostsResponseDto;
 import com.kcw0360.study.springboot.web.dto.PostsSaveRequestDto;
 import com.kcw0360.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,6 +32,12 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
---
+++ src/main/java/com/kcw0360/study/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.kcw0360.study.springboot.web.dto;
+
+import com.kcw0360.study.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
