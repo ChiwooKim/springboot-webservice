1aa28ae807e5ecbfd1db6375ccddcba11ddc2dcf
kcw0360 <kcw0360@naver.com>
2023-04-22 20:21:01 +0900

    기존 테스트에 시큐리티 적용

---
+++ src/main/java/com/kcw0360/study/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kcw0360.study.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
